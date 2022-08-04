918c2d1c4d7adb30a869a71058d0068bdc409f15
Carlos <36555654+lospoy@users.noreply.github.com>
2022-08-04 16:50:50 -0400

    update object model

--- backend/models/priceModel.js
+++ backend/models/priceModel.js
@@ -8,23 +8,37 @@ const priceSchema = mongoose.Schema(
             required: true,
             ref: 'User',
         },
+        // PRICE OF THE ITEM AND CURRENCY
         itemPrice: {
-            // price of the item stored in cents to avoid floats
-            type: Number,
+            // price of the item must be stored in cents to avoid floats
+            type: Object,
             required: [true, 'Item price required'],
+            properties: {
+              priceAmount: {
+                type: Number,
+                required: [true, 'Price amount must be specified'],
+              },
+              priceCurrency: {
+                type: String,
+                required: [true, 'Price currency must be specified'],
+              }
+            }
         },
-        priceCurrency: {
-            type: String,
-            required: [true, 'Currency must be specified'],
-        },
+        // MEASUREMENT UNIT AND THE AMOUNT
         itemQuantity: {
-            type: Number,
-            required: [true, 'Quantity required'],
-        },
-        quantityUnit: {
-            type: String,
-            required: [true, 'Unit of measure must be specified'],
-        },
+          type: Object,
+          required: [true, 'Item quantity required'],
+          properties: {
+            quantityAmount: {
+              type: Number,
+              required: [true, 'Quantity amount required'],
+            },
+            quantityUnit: {
+              type: String,
+              required: [true, 'Unit of measure must be specified'],
+            },
+          }
+      },
         tags: {
             type: String,
             required: [true, 'Please enter at least one tag'],
