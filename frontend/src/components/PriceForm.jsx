a389e63bf09b23b3af6d1eaad42d930baba246c2
Carlos <36555654+lospoy@users.noreply.github.com>
2022-08-08 13:22:57 -0400

    updated priceForm: working multiple inputs

--- frontend/src/components/PriceForm.jsx
+++ frontend/src/components/PriceForm.jsx
@@ -3,112 +3,86 @@ import { useDispatch } from 'react-redux'
 import { createPrice } from '../features/prices/priceSlice'
 
 function PriceForm() {
-    const [
-      priceAmount, setPriceAmount,
-      priceCurrency, setPriceCurrency,
-      quantityAmount, setQuantityAmount,
-      quantityUnit, setQuantityUnit,
-      tags, setTags
-    ] = useState('')
-    // const [
-    //   priceAmount, setPriceAmount,
-    //   priceCurrency, setPriceCurrency,
-    //   quantityAmount, setQuantityAmount,
-    //   quantityUnit, setQuantityUnit,
-    //   tags, setTags
-    // ] = useState('')
+    const [ state, setState ] = useState({
+      priceAmount: '',
+      priceCurrency: '',
+      quantityAmount: '',
+      quantityUnit: '',
+      tags: ''
+    })
 
     const dispatch = useDispatch()
 
-    // this onsubmit used to be set as
-    //   const onSubmit = (e) => {
-    //     e.preventDefault()
-    //     dispatch(createPrice({ text }))
-    //     setText('') }
-
-    // now it needs to create an object base on the price object schema
-    // issue is that it needs to be 2x (number+string object) and a tag >>
-    // JSON as follows
-
-    // {
-    //   "itemPrice": {
-    //       "priceAmount": 50,
-    //       "priceCurrency": "USD"
-    //   },
-    //   "itemQuantity": {
-    //       "quantityAmount": 10,
-    //       "quantityUnit": "lb"
-    //   },
-    //   "tags": "object"
-    // }
     const onSubmit = (e) => {
         e.preventDefault()
 
         dispatch(createPrice({ 
           itemPrice: {
-            priceAmount,
-            priceCurrency,
+            priceAmount: state.priceAmount,
+            priceCurrency: state.priceCurrency,
           },
           itemQuantity: {
-            quantityAmount,
-            quantityUnit,
+            quantityAmount: state.quantityAmount,
+            quantityUnit: state.quantityUnit
           },
-          tags,
+          tags: state.tags,
         }))
-        setPriceAmount('')
-        setPriceCurrency('')
-        setQuantityAmount('')
-        setQuantityUnit('')
-        setTags('')
+    }
+
+    const handleChange = (e) => { 
+      setState({
+        ...state,
+        [e.target.name]: e.target.value
+      })
     }
 
     return (
         <section className='form'>
-            <form onSubmit={onSubmit}>
-                <div className="form-group">
-                    <label htmlFor="priceAmount">Price</label>
+            <form onSubmit={ onSubmit }>
+                <div className='form-group'>
+                    <label htmlFor='priceAmount'>Price</label>
                     <input
                         type='number'
                         name='priceAmount'
                         id='priceAmount'
-                        value={priceAmount}
-                        onChange={(e) => setPriceAmount(e.target.value)}
+                        value={ state.priceAmount }
+                        onChange={ handleChange }
                     />
-                    <label htmlFor="priceCurrency">Currency</label>
+                    <label htmlFor='priceCurrency'>Currency</label>
                     <input
                         type='text'
                         name='priceCurrency'
                         id='priceCurrency'
-                        value={priceCurrency}
-                        onChange={(e) => setPriceCurrency(e.target.value)}
+                        value={ state.priceCurrency}
+                        onChange={ handleChange }
                     />
-                    <label htmlFor="priceCurrency">Quantity</label>
+                    <label htmlFor='priceCurrency'>Quantity</label>
                     <input
                         type='number'
                         name='quantityAmount'
                         id='quantityAmount'
-                        value={quantityAmount}
-                        onChange={(e) => setQuantityAmount(e.target.value)}
+                        value={ state.quantityAmount}
+                        onChange={ handleChange }
                     />
-                    <label htmlFor="quantityUnit">Unit</label>
+                    <label htmlFor='quantityUnit'>Unit</label>
                     <input
                         type='text'
                         name='quantityUnit'
                         id='quantityUnit'
-                        value={quantityUnit}
-                        onChange={(e) => setQuantityUnit(e.target.value)}
+                        value={ state.quantityUnit}
+                        onChange={ handleChange }
                     />
-                    <label htmlFor="tags">Tags</label>
+                    <label htmlFor='tags'>Tags</label>
                     <input
                         type='text'
                         name='tags'
                         id='tags'
-                        value={tags}
-                        onChange={(e) => setTags(e.target.value)}
+                        value={ state.tags}
+                        onChange={ handleChange }
                     />
                 </div>
-                <div className="form-group">
-                    <button className="btn btn-block" type='submit'>
+                <div className='form-group'>
+                    <button className='btn btn-block' type='submit'>
                         Add Price
                     </button>
                 </div>
